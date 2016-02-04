136fb434ab7f14e0cffc9faa5112e571bbedba0a
hypegroup <mees@hypegroup.cf>
2016-02-04 23:27:21 +0100

    proper

--- Assets/Scripts/CustomController.cs
+++ Assets/Scripts/CustomController.cs
@@ -1,47 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class CustomController : MonoBehaviour {
-    
+public class CustomController : NetworkBehaviour
+{
+
     public float groundCheckDistance;
     public float abovestart;
 
-    public Vector3 relative;
+    private Vector3 relative;
     private Vector3 startpos;
     private Animator anim;
     private bool isGrounded;
     private CharacterController cc;
 
-    public Vector3 customvelocity;
-    Vector3 currentpos;
-    Vector3 lastpos;
 
     int llayerMask = 1 << 8;
 
-    // Use this for initialization
-    void Start () {
+    void Start()
+    {
         cc = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
         llayerMask = ~llayerMask;
-        currentpos = transform.position;
-        lastpos = currentpos;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    void Update()
+    {
 
         relative = transform.InverseTransformDirection(cc.velocity);
-        // Debug.Log(relative);
-                    //customvelocity;
-
         checkGrounded();
 
-        // if (cc.isGrounded | NetworkScript.grounded)
         if (isGrounded)
         {
             anim.SetBool("Grounded", true);
-            anim.SetFloat("xspeed", relative.x / 5);
-            anim.SetFloat("zspeed", relative.z / 5);
+            if (isLocalPlayer)
+            {
+                anim.SetFloat("xspeed", relative.x / 5);
+                anim.SetFloat("zspeed", relative.z / 5);
+            }
         }
         else
         {
@@ -49,43 +46,21 @@ public class CustomController : MonoBehaviour {
         }
         anim.SetFloat("yspeed", relative.y);
 
-            
+
     }
-    //void FixedUpdate()
-    //{
-    //    CalcCurrentPos();
-    //}
     void checkGrounded()
     {
         startpos = transform.position + new Vector3(0, abovestart, 0);
 
         RaycastHit hit;
-        if (Physics.Raycast(startpos, -Vector3.up, out hit, maxDistance: (groundCheckDistance + abovestart), layerMask:llayerMask))
+        if (Physics.Raycast(startpos, -Vector3.up, out hit, maxDistance: (groundCheckDistance + abovestart), layerMask: llayerMask))
         {
-            Debug.Log(hit.collider.tag);
-            if (hit.collider.tag == "Ground")
-            {
-                isGrounded = true;
-            }
-            else
-            {
-                isGrounded = false;
-            }
+            isGrounded = (hit.collider.tag == "Ground") ? true : false;
+
         }
         else
         {
             isGrounded = false;
         }
     }
-    //void CalcCurrentPos()
-    //{
-    //    currentpos = transform.position;
-    //    customvelocity = transform.InverseTransformDirection(currentpos - lastpos) / Time.deltaTime;
-    //    customvelocity.z = (customvelocity.z < 0.001f && customvelocity.z > -0.001f) ? 0: customvelocity.z ;
-    //    customvelocity.x = (customvelocity.x < 0.001f && customvelocity.x > -0.001f) ? 0: customvelocity.x ;
-    //    customvelocity.y = (customvelocity.y < 0.001f && customvelocity.y > -0.001f) ? 0: customvelocity.y ;
-    //    lastpos = currentpos;
-    //}
-
-
 }
