1f6732b3da67786e37f63b26fc03e8028582f5d9
Colin de Roos <colinderoos@hotmail.nl>
2016-02-19 16:48:31 +0100

    added escape menu

---
+++ Assets/EscListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscListener : MonoBehaviour {
+	GameObject guiCanvas;
+	// Use this for initialization
+	void Start () {
+		guiCanvas = GameObject.Find ("Canvas");
+		guiCanvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			if(guiCanvas.active){
+				guiCanvas.SetActive (false);
+			}else{
+				guiCanvas.SetActive(true);
+			}
+		}
+	}
+}
