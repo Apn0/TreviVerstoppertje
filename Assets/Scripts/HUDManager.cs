dd71d18644ecda292a695e2d398a6b0e4d290d60
Colin de Roos <colinderoos@hotmail.nl>
2016-02-29 20:53:16 +0100

    tried stuff but didn't get an inch further

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -7,6 +7,7 @@ public class HUDManager : MonoBehaviour {
     private Slider hpbar;
     private int playerhp;
     private Camera cam;
+    private Player player;
 	// Update is called once per frame
 
     public void UpdatePlayerHealth (int hp)
@@ -18,18 +19,23 @@ public class HUDManager : MonoBehaviour {
         hpbar.value = hp;
     }
 
-    void onGui()
+    void OnGUI()
     {
         foreach (System.Collections.Generic.KeyValuePair<string, Player> kvp in GameManager.GetPlayers())
         {
-            cam = kvp.Value.GetComponentInChildren<Camera>();
-            GUI.color = Color.green;
-            Vector3 pos = cam.WorldToViewportPoint(kvp.Value.transform.position);
-            Vector3 pos1 = kvp.Value.transform.position = cam.transform.position;
+            player = kvp.Value;
+            GUI.color = Color.red;
+
+            Vector3 pos = cam.WorldToViewportPoint(player.transform.position);
+            /*Vector3 pos1 = kvp.Value.transform.position - cam.transform.position;
             if(Vector3.Dot(cam.transform.forward, pos1) > 0)
             {
                 GUI.Label(new Rect(pos.x, (Screen.height - pos.y) - pos.z / cam.fieldOfView, 155, 155), kvp.Value.transform.name);
-            }
+            }*/
+
+            //Debug.Log(pos.x + ", " + pos.y);
+            GUI.Label(new Rect(pos.x, pos.y, 155, 155), player.GetComponent<Player>().playerName);
+
             GUI.color = Color.black;
         }
     }
