b2d5fdb678b631b87b108f8494b2e15e554b9d6e
hypegroup <mees@hypegroup.cf>
2016-03-05 15:16:21 +0100

    bullethole

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -3,59 +3,72 @@ using UnityEngine.Networking;
 
 public class PlayerShoot : NetworkBehaviour {
 
-	private const string PLAYER_TAG = "Player";
+    private const string PLAYER_TAG = "Player";
 
-	public PlayerWeapon weapon;
+    public PlayerWeapon weapon;
 
-	[SerializeField]
-	private Camera cam;
+    public GameObject bulletHolePrefab;
 
-	[SerializeField]
-	private LayerMask mask;
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
 
     private float t = 0f;
 
 
-	void Start ()
-	{
-		if (cam == null)
-		{
-			Debug.LogError("PlayerShoot: No camera referenced!");
-			this.enabled = false;
-		}
-	}
+    void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
 
-	void Update ()
-	{
+    void Update()
+    {
         t += Time.deltaTime;
-		if (Input.GetButton("Fire1") && weapon.guncooldown < t )
-		{
-			Shoot();
+        if (Input.GetButton("Fire1") && weapon.guncooldown < t)
+        {
+            Shoot();
             t = 0;
-		}
-	}
+        }
+    }
 
-	[Client]
-	void Shoot ()
-	{
-		RaycastHit _hit;
-		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask) )
-		{
-			if (_hit.collider.tag == PLAYER_TAG)
-			{
-				CmdPlayerShot(_hit.collider.name, weapon.damage);
-			}
-		}
+    [Client]
+    void Shoot()
+    {
+        CmdShootFX();
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        {
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(_hit.collider.name, weapon.damage);
+            }
+            else
+            {
+                GameObject hole = (GameObject)GameObject.Instantiate(bulletHolePrefab, (_hit.point + _hit.normal * 0.01f), Quaternion.FromToRotation(-Vector3.forward, _hit.normal));
+            }
+        }
 
-	}
+    }
 
-	[Command]
-	void CmdPlayerShot (string _playerID, int _damage)
-	{
-		Debug.Log(_playerID + " has been shot.");
+    [Command]
+    void CmdPlayerShot(string _playerID, int _damage)
+    {
+        Debug.Log(_playerID + " has been shot.");
 
         Player _player = GameManager.GetPlayer(_playerID);
         _player.RpcTakeDamage(_damage, GetComponent<Player>().playerName);
-	}
+    }
+
+    [Command]
+    void CmdShootFX()
+    {
+
+    }
 
 }
