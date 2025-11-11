3708d71fd67246d6ebd5ca232e070cee479121ae
Apn0 <arno13579@gmail.com>
2025-11-11 09:22:15 +0100

    Add files via upload

---
+++ tour.js
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="nl" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> :: Wil je meer over Trevianum weten?</title>
+    <meta name="description" content="Wil je meer over Trevianum weten? Dan ben je hier op de juiste plek! We hebben voor jou en natuurlijk ook voor je ouders of verzorgers een digitale tour door onze school gemaakt. Zo kun je Trevianum vanuit thuis al goed leren kennen. Bezoek de verschillende vakken in hun eigen leslokaal, loop door de hal en de gangen, bekijk de aula en klik vooral op de buttons want dan ontdek je pas echt wat Trevianum allemaal te bieden heeft.">
+    <meta name="keywords" content="">
+    <link rel=”canonical” href=”https://naartrevianum.nl/virtual-tour-contact/tour.js”>
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/_images/settings/21/32-pijltjes-rechts.png?1610706732">
+    <link rel="icon" type="image/png" sizes="16x16" href="/_images/settings/21/16-pijltjes-rechts.png?1610706732">
+    <link rel="shortcut icon" href="/_images/settings/21/pijltjes-rechts.ico?1610706732">
+    <link rel="manifest" href="/_images/settings/21/site.webmanifest?1610706732">
+    <meta property="og:locale" content="nl_NL"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content=" :: Wil je meer over Trevianum weten?"/>
+    <meta property="og:description" content="Wil je meer over Trevianum weten? Dan ben je hier op de juiste plek! We hebben voor jou en natuurlijk ook voor je ouders of verzorgers een digitale tour door onze school gemaakt. Zo kun je Trevianum vanuit thuis al goed leren kennen. Bezoek de verschillende vakken in hun eigen leslokaal, loop door de hal en de gangen, bekijk de aula en klik vooral op de buttons want dan ontdek je pas echt wat Trevianum allemaal te bieden heeft."/>
+    <meta property="og:site_name" content="Naar Trevianum"/>
+    <meta property="og:image" content="https://naartrevianum.nl/_images/settings/9/schermafbeelding 2023-09-13 170527.png"/>
+    <meta property="og:url" content="https://naartrevianum.nl/virtual-tour-contact/tour.js"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:description" content="Wil je meer over Trevianum weten? Dan ben je hier op de juiste plek! We hebben voor jou en natuurlijk ook voor je ouders of verzorgers een digitale tour door onze school gemaakt. Zo kun je Trevianum vanuit thuis al goed leren kennen. Bezoek de verschillende vakken in hun eigen leslokaal, loop door de hal en de gangen, bekijk de aula en klik vooral op de buttons want dan ontdek je pas echt wat Trevianum allemaal te bieden heeft."/>
+    <meta name="twitter:title" content=" :: Wil je meer over Trevianum weten?"/>
+    <meta name="twitter:image" content="https://naartrevianum.nl/_images/settings/9/schermafbeelding 2023-09-13 170527.png"/>
+    <meta name="twitter:creator" content=""/>
+    <meta name="theme-color" content="#1c64f2"/>
+
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "url": "https://naartrevianum.nl",
+            "logo": "https://naartrevianum.nl/_images/settings/9/schermafbeelding 2023-09-13 170527.png",
+            "name": "Naar Trevianum",
+            "email": "informatie@trevianum.nl",
+            "telephone": "+31464206900",
+            "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+31464206900",
+                "email": "informatie@trevianum.nl",
+                "contactType": "customer support"
+            },
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Sittard, Netherlands",
+                "addressRegion": "LB",
+                "postalCode": "6135 CV",
+                "streetAddress": "Bradleystraat 25"
+            }
+        }
+    </script>
+
+            <link rel="stylesheet" href="/_css/bootstrap.5.0.0-beta1.css">
+        <link rel="stylesheet" href="/_css/main.css?7">
+    </head>
+<body class="d-flex flex-column minh-100 position-relative">
+<div class="container-fluid g-0 flex-grow-1 d-flex flex-column flex-lg-row">
+    <header class="pe-0 pe-lg-4 shadow ">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white flex-row flex-lg-column align-items-center align-items-lg-start py-2 py-lg-0">
+            <a class="navbar-brand mt-0 mt-lg-1" href="/"> <img src="/_img/logo-trevianum.png" width="250" alt="Trevianum logo"/> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse ps-4 pe-4 pe-lg-0" id="navbarNav">
+                                <ul class="navbar-nav flex-column mt-0 mt-lg-5">
+                                            <li class="nav-item">
+                            <a class="nav-link" href="/" >
+                                Home                            </a>
+                                                    </li>
+                                            <li class="nav-item">
+                            <a class="nav-link" href="/Trevianum Academie" >
+                                Trevianum Academie Junior                            </a>
+                                                    </li>
+                                            <li class="nav-item dropdown dropend">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="sub2">
+                                Trevianum                            </a>
+                                                            <div class="dropdown-menu" aria-labelledby="sub2">
+                                                                            <a class="dropdown-item " href="/trevianum/algemeen">Algemeen</a>
+                                                                            <a class="dropdown-item " href="/tweejarigebrugklas">Verlengde brugklas</a>
+                                                                            <a class="dropdown-item " href="/trevianum/drie-scholen-onder-een-dak">Drie scholen onder één dak</a>
+                                                                            <a class="dropdown-item " href="/trevianum/havo">Havo</a>
+                                                                            <a class="dropdown-item " href="/trevianum/atheneum">Atheneum</a>
+                                                                            <a class="dropdown-item " href="/trevianum/gymnasium">Gymnasium</a>
+                                                                    </div>
+                                                    </li>
+                                            <li class="nav-item">
+                            <a class="nav-link" href="/uitdaging" >
+                                Extra uitdaging                            </a>
+                                                    </li>
+                                            <li class="nav-item">
+                            <a class="nav-link" href="/stromen" >
+                                Stromen                            </a>
+                                                    </li>
+                                            <li class="nav-item dropdown dropend">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="sub5">
+                                Topsport Talentschool                            </a>
+                                                            <div class="dropdown-menu" aria-labelledby="sub5">
+                                                                            <a class="dropdown-item " href="/topsport-talentschool/algemeen">Algemeen</a>
+                                                                            <a class="dropdown-item " href="/topsport-talentschool/topsport-talent">Topsport talent</a>
+                                                                            <a class="dropdown-item " href="/topsport-talentschool/talentherkenning">Talentherkenning</a>
+                                                                            <a class="dropdown-item " href="/topsport-talentschool/inschrijven">Inschrijven TTS</a>
+                                                                    </div>
+                                                    </li>
+                                            <li class="nav-item dropdown dropend">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="sub6">
+                                Kennismaken & inschrijven                            </a>
+                                                            <div class="dropdown-menu" aria-labelledby="sub6">
+                                                                            <a class="dropdown-item " href="/kennismaken/algemeen">Algemeen</a>
+                                                                            <a class="dropdown-item " href="/kennismaken/opendag">Open dag</a>
+                                                                            <a class="dropdown-item " href="/kennismaken/proeflessen">Proeflessen en infoavond</a>
+                                                                            <a class="dropdown-item " href="/kennismaken/inschrijven">Inschrijven voorlichtingsactiviteiten</a>
+                                                                    </div>
+                                                    </li>
+                                            <li class="nav-item">
+                            <a class="nav-link" href="/veelgestelde-vragen" >
+                                Veelgestelde vragen                            </a>
+                                                    </li>
+                                        <div class="pt-3 mt-2 border-top">
+                        <ul class="navbar-nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="https://naartrevianum.nl/virtual-tour-contact/index.html" target="_blank">
+                                        Digitale Tour
+                                    </a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link" href="/filmpjes-digitale-tour">
+                                        Filmpjes Digitale Tour                                    </a>
+                                </li>
+                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="/informatieavond-gemist">
+                                    Informatieavond gemist?
+                                </a>
+                            </li>
+
+                            <li class="nav-item ">
+                                <a class="nav-link" href="/contact">
+                                    Contact                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="m-3 m-lg-5 flex-grow-1">
+<div class="container my-5">    <div class="px-md-5 px-2">        <div class="row">            <div class="col-12">                <h1>Whoops!!</h1>                <h2>Pagina niet gevonden</h2>                <p>Het ziet ernaar uit dat je niet gevonden hebt waar je voor kwam. Probeer het menu hierboven eens?</p>            </div>        </div>    </div></div>    </main>
+</div>
+
+<footer>
+    <div class="container-fluid py-5">
+        <div class="d-flex justify-content-center">
+                <img src="/_img/logo-trevianum-wit.png" alt="Trevianum Scholengroep" class="img-fluid trevianumfooter">
+                    </div>
+    </div>
+</footer>
+<div class="superfooter py-2">
+    <div class="container">
+            <div class="smaller text-center mb-2 text-cyaan">
+                Design <a href="https://studiomarq.nl" rel="noopener" target="_blank">Studiomarq</a> &bull;
+                VR Tour <a href="https://studiovr.nl" rel="noopener" target="_blank">Studio VR</a> &bull;
+                Ontwikkeling <a href="https://schmeits.com" rel="noopener" target="_blank">Schmeits websolutions</a>
+            </div>
+            <div class="small text-center">
+                &copy; Trevianum scholengroep 2025
+            </div>
+        </small>
+    </div>
+</div>
+
+
+
+<script src="/_js/plugins.js?1"></script>
+<script src="/_js/main.js?1"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-788NWCYKG2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-788NWCYKG2', { 'anonymize_ip': true } );
+    </script>
+
+</body>
+</html>
